bd4d7006b658db2c6949868d8f03acf32e768001
Gwrzlh <dafarizqulloh138@gmail.com>
2025-08-04 14:40:26 +0700

    membuat function tambah pengguna admin dan alumni

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,3 +16,8 @@ $routes->get('/admin/pengguna','penggunaController::index');
 $routes->get('/admin/pengguna/tambahPengguna','penggunaController::create');
 $routes->post('/admin/pengguna/tambahPengguna/post','penggunaController::store');
 
+//ajax route
+$routes->group('api', function($routes) {
+    $routes->get('cities/province/(:num)', 'penggunaController::getCitiesByProvince/$1');
+});
+
