c99a94de310f9b2b58702b9f724fd75d80f848a5
Cowboys527 <nazarabdillah73@gmail.com>
2025-08-05 14:39:47 +0700

    update bagian pengguna

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,6 +18,8 @@ $routes->get('/admin', 'adminController::index');
 $routes->get('/admin/pengguna', 'penggunaController::index');
 $routes->get('/admin/pengguna/tambahPengguna', 'penggunaController::create');
 $routes->post('/admin/pengguna/tambahPengguna/post', 'penggunaController::store');
+$routes->get('admin/dashboard', 'AdminController::dashboard');
+
 //route ajax 
 $routes->group('api', function($routes) {
     $routes->get('cities/province/(:num)', 'penggunaController::getCitiesByProvince/$1');
