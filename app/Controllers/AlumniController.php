c1c418e759baad56563541bcbe01b1432601e234
Rere <rerevutra@email.com>
2025-08-21 15:15:23 +0700

    untuk supervisi

--- app/Controllers/AlumniController.php
+++ app/Controllers/AlumniController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use App\Models\DetailaccountAlumni;
+
 class AlumniController extends BaseController
 {
     public function dashboard()
@@ -14,44 +17,78 @@ class AlumniController extends BaseController
 
         return view('alumni/supervisi');
     }
-
-    public function isi()
+    public function lihatTeman()
     {
-        // Dummy data, nanti diganti dari DB
-        $data['alumni'] = [
-            ['no' => 1, 'nim' => '2021001', 'nama' => 'Budi', 'status' => 'Finish'],
-            ['no' => 2, 'nim' => '2021002', 'nama' => 'Siti', 'status' => 'Belum Mengisi'],
-            ['no' => 3, 'nim' => '2021003', 'nama' => 'Andi', 'status' => 'Ongoing'],
-        ];
+        $alumniModel = new DetailaccountAlumni();
 
-        return view('alumni/isi', $data);
-    }
+        // Ambil data alumni yang sedang login
+        $currentAlumni = $alumniModel
+            ->where('id_account', session('id'))
+            ->first();
 
-    public function teman()
-    {
-        // Dummy teman alumni
-        $data['teman'] = [
-            ['nim' => '2021004', 'nama' => 'Rina', 'prodi' => 'Teknik Industri'],
-            ['nim' => '2021005', 'nama' => 'Agus', 'prodi' => 'Teknik Mesin'],
-        ];
+        if (!$currentAlumni) {
+            return redirect()->back()->with('error', 'Data alumni tidak ditemukan.');
+        }
 
-        return view('alumni/teman', $data);
-    }
-
-    public function form($nim)
-    {
-        // Untuk contoh, kita cari data berdasarkan NIM dari dummy array
-        $alumni = [
-            '2021001' => ['nama' => 'Budi'],
-            '2021002' => ['nama' => 'Siti'],
-            '2021003' => ['nama' => 'Andi'],
-        ];
+        // Ambil alumni lain dengan jurusan & prodi sama + join status dari account
+        $teman = $alumniModel
+            ->select('detailaccount_alumni.*, account.status')
+            ->join('account', 'account.id = detailaccount_alumni.id_account')
+            ->where('id_jurusan', $currentAlumni['id_jurusan'])
+            ->where('id_prodi', $currentAlumni['id_prodi'])
+            ->where('detailaccount_alumni.id_account !=', session('id'))
+            ->findAll();
 
         $data = [
-            'nim' => $nim,
-            'nama' => $alumni[$nim]['nama'] ?? 'Tidak Diketahui'
+            'teman'   => $teman,
+            'jurusan' => $currentAlumni['id_jurusan'],
+            'prodi'   => $currentAlumni['id_prodi'],
         ];
 
-        return view('alumni/form', $data);
+        return view('alumni/lihat_teman', $data);
     }
+
+
+
+
+    // public function isi()
+    // {
+    //     // Dummy data, nanti diganti dari DB
+    //     $data['alumni'] = [
+    //         ['no' => 1, 'nim' => '2021001', 'nama' => 'Budi', 'status' => 'Finish'],
+    //         ['no' => 2, 'nim' => '2021002', 'nama' => 'Siti', 'status' => 'Belum Mengisi'],
+    //         ['no' => 3, 'nim' => '2021003', 'nama' => 'Andi', 'status' => 'Ongoing'],
+    //     ];
+
+    //     return view('alumni/isi', $data);
+    // }
+
+    // public function teman()
+    // {
+    //     // Dummy teman alumni
+    //     $data['teman'] = [
+    //         ['nim' => '2021004', 'nama' => 'Rina', 'prodi' => 'Teknik Industri'],
+    //         ['nim' => '2021005', 'nama' => 'Agus', 'prodi' => 'Teknik Mesin'],
+    //     ];
+
+    //     return view('alumni/teman', $data);
+    // }
+
+    // public function form($nim)
+    // {
+    //     // Untuk contoh, kita cari data berdasarkan NIM dari dummy array
+    //     $alumni = [
+    //         '2021001' => ['nama' => 'Budi'],
+    //         '2021002' => ['nama' => 'Siti'],
+    //         '2021003' => ['nama' => 'Andi'],
+    //     ];
+
+    //     $data = [
+    //         'nim' => $nim,
+    //         'nama' => $alumni[$nim]['nama'] ?? 'Tidak Diketahui'
+    //     ];
+
+    //     return view('alumni/form', $data);
+    // }
+
 }
