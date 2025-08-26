0a4ae5602541d46f42d811e64afa6dbecaee02a4
Hafzfrds <hafizfirdaus26022008@gmail.com>
2025-08-26 13:29:46 +0700

    'penyesuaianstyle'

--- app/Views/adminpage/kontak/index.php
+++ app/Views/adminpage/kontak/index.php
@@ -8,6 +8,7 @@
     <title>Manajemen Kontak</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="<?= base_url('css/kontak.css') ?>">
 </head>
 
 <body class="p-4">
