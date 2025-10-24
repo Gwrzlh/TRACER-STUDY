79338aafa8abfc766faf5d3604048e1c18800112
Cowboys527 <nazarabdillah73@gmail.com>
2025-10-24 13:31:18 +0700

    style log dashboard dan style kuesioner atasan

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -190,14 +190,14 @@ $currentRoute = service('request')->uri->getPath();
         <form action="/logout" method="get">
           <button type="submit"
             style="background-color: <?= get_setting('logout_button_color', '#dc3545') ?>;
-             color: <?= get_setting('logout_button_text_color', '#ffffff') ?>;
-             padding: 10px 20px;
-             font-weight: 600;
-             border-radius: 8px;
-             width: 100%; text-align:center;"
+                   color: <?= get_setting('logout_button_text_color', '#ffffff') ?>;
+                   padding: 10px 20px;
+                   font-weight: 600;
+                   border-radius: 8px;
+                   width: 100%; text-align:center;"
             onmouseover="this.style.backgroundColor='<?= get_setting('logout_button_hover_color', '#a71d2a') ?>';"
             onmouseout="this.style.backgroundColor='<?= get_setting('logout_button_color', '#dc3545') ?>';">
-            <?= esc(get_setting('logout_button_text', 'Logout')) ?>
+            Logout
           </button>
         </form>
       </div>
